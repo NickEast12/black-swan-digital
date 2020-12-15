d9f13274f58ede4a2d82cd6a856c1960853d786c
Nick <nickeast1998@gmail.com>
2020-12-15 13:12:49 +0000

    alt services added to home page

---
+++ src/components/pageComponents/altServices.js
@@ -0,0 +1,113 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+//* SVGs
+import WebDevIcon from '../../svgs/web-development.svg';
+import DesignIcon from '../../svgs/web-design.svg';
+import SEOIcon from '../../svgs/responsive-design.svg';
+import MarketingIcon from '../../svgs/digital-marketing.svg';
+
+const AltServicesStyles = styled.div`
+  width: 100%;
+  padding: 2rem 0;
+  .offerings__wrapper {
+    width: 80%;
+    margin: 0 auto;
+    text-align: center;
+    &__content {
+      span {
+        color: var(--mainColour);
+        font-weight: 700;
+        font-size: 1.1rem;
+        letter-spacing: -0.9px;
+        text-transform: uppercase;
+        padding-bottom: 0.25rem;
+        display: block;
+      }
+      h2 {
+        font-size: 1.5rem;
+        margin: 0.5rem 0 1rem 0;
+      }
+      p {
+        margin-bottom: 1rem;
+      }
+    }
+    &__boxes {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 1rem;
+      margin-top: 2rem;
+      section {
+        border-radius: 4.5px;
+        background: var(--white);
+        box-shadow: 0 0 47px -1px rgba(153, 153, 153, 0.2);
+        padding: 1.5rem;
+        a {
+          text-decoration: none;
+          svg {
+            width: 50px;
+          }
+          h6 {
+            font-size: 1rem;
+            margin-top: 0.5rem;
+            font-weight: 500;
+          }
+        }
+      }
+    }
+    &__btn {
+      margin-top: 2rem;
+      button {
+        width: 125px;
+      }
+    }
+  }
+`;
+const AltServices = () => (
+  <AltServicesStyles>
+    <div className="offerings__wrapper">
+      <div className="offerings__wrapper__content">
+        <span>Our Services</span>
+        <h2>Our full range of web development services.</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia incidunt
+          nostrum nihil! Fugit dignissimos sequi quos numquam maiores possimus
+          molestias voluptate vero perferendis, labore eveniet!
+        </p>
+      </div>
+      <div className="offerings__wrapper__boxes">
+        <section>
+          <Link to="/">
+            <WebDevIcon />
+            <h6>Web Development</h6>
+          </Link>
+        </section>
+        <section>
+          <Link to="/">
+            <DesignIcon />
+            <h6>Branding &amp; Design</h6>
+          </Link>
+        </section>
+        <section>
+          <Link to="/">
+            <SEOIcon />
+            <h6>SEO</h6>
+          </Link>
+        </section>
+        <section>
+          <Link to="/">
+            <MarketingIcon />
+            <h6>Digital Marketing</h6>
+          </Link>
+        </section>
+      </div>
+      <div className="offerings__wrapper__btn">
+        <Link to="/services">
+          <button type="button">Read More</button>
+        </Link>
+      </div>
+    </div>
+  </AltServicesStyles>
+);
+
+export default AltServices;
